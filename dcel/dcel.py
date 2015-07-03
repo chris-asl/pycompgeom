63e0fe9d789c2e1bb14d96b1e7c7946b6e26ebfe
chris-asl <chris.aslanoglou@gmail.com>
2015-07-03 16:53:36 +0300

    Add HalfEdge, Vertex classes methods, add Face class, add least_cw_segment method, and DCEL helper data members
    	Changelog:
    		[*] All classes: Add checks for TypeErrors
    		[*] All classes: Changed constructor arguments
    		[*] HalfEdge: Add set_next, set_previous, set_twin
    		[*] Vertex: Add add_incident_edge
    		[*] Face: Add class and methods: set_inner_component
    		[*] DCEL: Add helper data members for the construction (will be deleted at the end of DCEL creation)
    		[*] DCEL: Add actual DCEL data members (faces, vertices, half-edges)
    		[*] DCEL: Add least_cw_segment (given a segment, finds the one that defines the max angle in a CW manner)

--- dcel/dcel.py
+++ dcel/dcel.py
@@ -1,33 +1,82 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = 'Chris Aslanoglou'
-from pycompgeom.primitives import Segment2
-from pycompgeom.pycompgeom import Point2
 from pycompgeom.visuals import *
-from dcel_gather_input import DcelInputData, similar_edges
+from dcel_gather_input import DcelInputData, similar_edges, is_segment_of_ch
+
 
 class HalfEdge(Segment2):
-    def __init__(self, origin, next_half_edge, previous_half_edge, twin_half_edge):
+    def __init__(self, origin, segment):
+        """
+        Construction of a HalfEdge. Next, previous and twin data members are set to None and must be set!
+        :param origin: Vertex The origin of the HalfEdge
+        :param segment: Segment2
+        """
+        if type(origin) is not Vertex:
+            raise TypeError("HalfEdge - __init__: Expected type \"Vertex\" for origin, got \"" + str(type(origin)))
+        if type(segment) is not Segment2:
+            raise TypeError("HalfEdge - __init__: Expected type \"Segment2\" for superclass, got \"" +
+                            str(type(segment)))
         self.origin = origin
-        self.next_half_edge = next_half_edge
-        self.previous_half_edge = previous_half_edge
-        self.twin = twin_half_edge
-        if self.twin is None:
-            raise ValueError("Twin half-edge is None")
-        else:
-            super(HalfEdge, self).__init__(origin, self.twin.origin)
+        self.next = None
+        self.previous = None
+        self.twin = None
+        other_point = segment.start if segment.start != origin else segment.end
+        super(HalfEdge, self).__init__(origin, other_point)
 
     def __repr__(self):
-        return "Half-Edge[(%s) -> (%s)]" % (self.origin, self.twin.origin)
+        return "Half-Edge[Origin: (%s)]" % self.origin
+
+    def set_next(self, he):
+        """Sets the next HE of the current one.
+        Also, sets the previous of the given one"""
+        if self.next is not None:
+            raise ValueError("HalfEdge - set_next: Next wasn't none - Possible attempt for erroneous overwrite")
+        if type(he) is not HalfEdge:
+            raise TypeError("HalfEdge - set_next: Expected type \"HalfEdge\", got \"" + str(type(he)) + "\"")
+        self.next = he
+        he.previous = self
+
+    def set_previous(self, he):
+        """Sets the previous HE of the current one.
+        Also, sets the next of the given one"""
+        if self.previous is not None:
+            raise ValueError("HalfEdge - set_previous: Previous wasn't none - Possible attempt for erroneous overwrite")
+        if type(he) is not HalfEdge:
+            raise TypeError("HalfEdge - set_previous: Expected type \"HalfEdge\", got \"" + str(type(he)) + "\"")
+        self.previous = he
+        he.next = self
+
+    def set_twin(self, twin):
+        """Sets the twin relationship between two half-edges"""
+        if type(twin) is not HalfEdge:
+            raise TypeError("HalfEdge - set_next: Expected type \"HalfEdge\", got \"" + str(type(twin)) + "\"")
+        self.twin = twin
+        twin.twin = self
 
 
 class Vertex(Point2):
-    def __init__(self, point2, incident_edges):
-        self.incident_edges = incident_edges
+    def __init__(self, point2):
+        """
+
+        :param point2: Point2 The point for the construction of the superclass
+        """
+        if type(point2) is not Point2:
+            raise TypeError("Vertex - __init__: Expected type \"Point2\", got \"" + str(type(point2)) + "\"")
+        self.incident_edges = []
         super(Vertex, self).__init__(point2.x, point2.y)
 
+    def add_incident_edge(self, he):
+        """
+        Adds incident edges to the vertex
+        :param he: HalfEdge The incident half-edge to be added
+        """
+        if type(he) is not HalfEdge:
+            raise TypeError("Vertex - add_incident_edge: Expected type \"HalfEdge\", got \"" + str(type(he)) + "\"")
+        self.incident_edges.append(he)
+
     def visit_incident_edges(self):
-        """Prints incident edges (possibly in a CCW order)"""
+        """Generator for the incident edges of the vertice"""
         for edge in self.incident_edges:
             yield edge
 
@@ -35,18 +84,69 @@ class Vertex(Point2):
         return "Vertex(%s, %s)" % (self.x, self.y)
 
 
+class Face:
+    def __init__(self, outer_component):
+        """
+
+        :param outer_component: HalfEdge The outer component of the face - an arbitrary HalfEdge
+        """
+        self.outer_component = outer_component
+        self.inner_component = None
+
+    def set_inner_component(self, he):
+        """
+
+        :param he: HalfEdge The inner half-edge of the face
+        """
+        if type(he) is not HalfEdge:
+            raise TypeError("Face - set_inner_component: Expected type \"HalfEdge\", got \"" + type(he) + "\"")
+        self.inner_component = he
+
+    def __repr__(self):
+        return "Face [OuterComponent: %s, InnerComponent: %s]" % (self.outer_component, self.inner_component)
+
+    def walk_face(self):
+        """Returns a list of HalfEdges that surround the face"""
+        start_vertice = self.outer_component.origin
+        idx_edge = self.outer_component
+        half_edges = [self.outer_component]
+        while True:
+            idx_edge = idx_edge.next
+            half_edges.append(idx_edge)
+            if idx_edge.next.origin == start_vertice:
+                break
+        return half_edges
+
+
+def point_key_repr(x, y):
+    return str(x) + "," + str(y)
+
+
 class DCEL:
-    def __init__(self, vertices, edges, is_connected_graph, min_max_coords_tuple, bb_dist, v_vertices, v_edges):
-        self.vertices = vertices
-        self.edges = edges
+    def __init__(self, points, segments, is_connected_graph, min_max_coords_tuple, bb_dist, bb_segments_number,
+                 v_points, v_segments, ch_segments_dict):
+        # Data members from input
+        self.points = points
+        # Convert to points to dictionary from "x-coord,y-coord" -> Point2
+        self.points_dict = dict((k, v) for k, v in [(str(p.x) + "," + str(p.y), p) for p in points])
+        self.segments = segments
         self.is_connected_graph = is_connected_graph
         self.min_x = min_max_coords_tuple[0]
         self.min_y = min_max_coords_tuple[1]
         self.max_x = min_max_coords_tuple[2]
         self.max_y = min_max_coords_tuple[3]
         self.bb_dist = bb_dist
-        self.v_vertices = v_vertices
-        self.v_edges = v_edges
+        self.bb_segments_number = bb_segments_number
+        self.v_points = v_points
+        self.v_segments = v_segments
+        self.ch_segment_dict = ch_segments_dict
+        # The following dictionaries help for finding the next and previous HalfEdges that are on the ConvexHull
+        self.ch_he_by_origin_dict = {}
+        self.ch_he_by_dest_dict = {}
+        # Data members of DCEL
+        self.vertices = []
+        self.half_edges = []
+        self.faces = []
         # Testing
         # self.most_ccw_edge(edges[0])
         # self.most_cw_edge(edges[0])
@@ -75,16 +175,32 @@ class DCEL:
         min_angle = 2 * math.pi
         result = None
         for e in edges:
-            angle = vectors_angle(edge, e)
-            if cw(edge.start, edge.end, e.end) and angle < min_angle:
+            angle = vectors_angle(segment, e)
+            if cw(segment.start, segment.end, e.end) and angle < min_angle:
                 min_angle = angle
                 result = e
-        self.v_edges.append(VSegment2(result, BLUE))
+        self.v_segments.append(VSegment2(result, BLUE))
         return result
+
+    def least_cw_segment(self, segment):
+        """Returns a segment that defines the maximum angle with the given segment
+        The segments are also connected in a CW manner"""
+        edges = self.find_connected_segments_with_segment(segment)
+        max_angle = 0
+        result = None
+        for e in edges:
+            angle = vectors_angle(segment, e)
+            if cw(segment.start, segment.end, e.end) and angle > max_angle:
+                max_angle = angle
+                result = e
+        self.v_segments.append(VSegment2(result, BLUE))
+        return result
+
 
 def dot_product(v1, v2):
     """Calculates the dot product of 2D vectors v1 and v2"""
-    return sum((a*b) for a, b in zip(v1, v2))
+    return sum((a * b) for a, b in zip(v1, v2))
+
 
 def vectors_angle(v1, v2):
     """Calculates the angle of 2D vectors v1 and v2, after translating them to origin"""
@@ -110,10 +226,10 @@ def vectors_angle(v1, v2):
 
 if __name__ == '__main__':
     dcel_data = DcelInputData()
-    vertices_pack, edges_pack, min_max_coords_tuple, bb_dist = dcel_data.get_visual_dcel_members()
+    vertices_pack, edges_pack, min_max_coords_tuple = dcel_data.get_visual_dcel_members()
     print dcel_data
     print "Done with DCELInputGather"
-    dcel = DCEL(vertices_pack[0], edges_pack[0], dcel_data.is_connected_graph, min_max_coords_tuple, bb_dist,
-                vertices_pack[1], edges_pack[1])
+    dcel = DCEL(vertices_pack[0], edges_pack[0], dcel_data.is_connected_graph, min_max_coords_tuple, dcel_data.bb_dist,
+                dcel_data.bb_edges_number, vertices_pack[1], edges_pack[1], dcel_data.ch_segments_dict)
     pause()
     del vertices_pack, edges_pack, dcel_data
